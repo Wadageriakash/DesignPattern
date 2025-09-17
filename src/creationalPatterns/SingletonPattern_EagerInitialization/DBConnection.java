6d1710ecaa0a5421460ebe998e4b245eb7c45b2b
Akash Wadageri <wadagireakash92@gmail.com>
2025-09-17 21:56:07 +0530

    updated code for the db connection for the Desingn patterns

--- src/creationalPatterns/SingletonPattern_EagerInitialization/DBConnection.java
+++ src/creationalPatterns/SingletonPattern_EagerInitialization/DBConnection.java
@@ -2,12 +2,15 @@ package creationalPatterns.SingletonPattern_EagerInitialization;
 
 public class DBConnection {
 
+	// Step 1: Create the object immediately (eagerly)
 	private static DBConnection connectionObj = new DBConnection();
 	
+	 // Step 2: Make constructor private so no one else can create new objects
 	private DBConnection() {
 		
 	}
 	
+	// Step 3: Provide a method to give the already created object
 	public static DBConnection getInstance() {
 		return connectionObj;
 	}
